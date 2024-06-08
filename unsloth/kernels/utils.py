b2b15b2842cb8f6207652df285d4aa5f6e62eebf
Ryan Witzman <92187780+ryanwitzman@users.noreply.github.com>
2024-06-08 03:48:15 -0400

    Update utils.py

--- unsloth/kernels/utils.py
+++ unsloth/kernels/utils.py
@@ -35,9 +35,9 @@ import ctypes
 import torch
 cdequantize_blockwise_fp32      = bnb.functional.lib.cdequantize_blockwise_fp32
 cdequantize_blockwise_fp16_nf4  = bnb.functional.lib.cdequantize_blockwise_fp16_nf4
-cdequantize_blockwise_bf16_nf4  = bnb.functional.lib.cdequantize_blockwise_bf16_nf4
+cdequantize_blockwise_bf16_nf4  = bnb.functional.lib.cgemm_4bit_inference_naive_fp16
 cgemm_4bit_inference_naive_fp16 = bnb.functional.lib.cgemm_4bit_inference_naive_fp16
-cgemm_4bit_inference_naive_bf16 = bnb.functional.lib.cgemm_4bit_inference_naive_bf16
+cgemm_4bit_inference_naive_bf16 = bnb.functional.lib.cgemm_4bit_inference_naive_fp16
 
 
 def QUANT_STATE(W):
